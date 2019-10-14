e1f9ee6c9bb349b2841c491a40b364e46e848d28
pankajm18 <43561999+pankajm18@users.noreply.github.com>
2019-10-14 18:59:30 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define deb(x) cout<<#x<<" "<<x<<endl
+int main()
+{
+    fastIO;
+    ll a,b;
+    cin>>a>>b;ll c=a+b;cout<<c<<"\n";   
+    return 0;
+}
